9675ef1cbeed05b9fa8e8758828be65e587925bd
Raphael Mounier <mounierr07@gmail.com>
2024-11-13 19:36:23 +0100

    kirin710: add support of harry device

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter potter, $(TARGET_DEVICE)),)
+ifneq ($(filter potter harry, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
